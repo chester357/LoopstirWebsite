6b15ebaae43e8f816f5228f9b04566da376d68be
Chester Schofield <chester35711@gmail.com>
2015-12-18 19:19:52 -0600

    about to restyle

--- public/scripts/homeController.js
+++ public/scripts/homeController.js
@@ -1,6 +1,37 @@
-angular.module('Loopstir');
+var app = angular.module('Loopstir');
 
-app.controller('homeController', ['$rootScope','$scope', function($rootScope, $scope) {
+app.controller('homeController', ['$rootScope','$scope', '$http', function($rootScope, $scope, $http) {
 
-    }
-]);
+    console.log("Wired up");
+
+    $scope.contact = {
+        name: 'unknown',
+        email: ''
+    };
+
+    $scope.addContact = function(contact) {
+        console.log(contact);
+        $http({
+            method: 'POST',
+            url: 'http://54.152.165.228/api/Account/Contact',
+            data: {
+                Email: contact.email,
+                Name: 'testing'
+            }
+        }).then(function successCallback(response) {
+            console.log('Success');
+            console.log(response);
+
+            if (response.status == 200) {
+
+                // success, notify UI
+
+            }
+
+        }, function errorCallback(response) {
+            console.log('Failure');
+            console.log(response);
+        });
+    };
+
+}]);
